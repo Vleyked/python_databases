50b0ccf41ae5b76f35b35afbb5d4aeacc96a94e2
victorcid <vcfiguera@gmail.com>
2023-08-13 14:01:35 +0300

    exercise1 ORM SQLAlchemy

---
+++ db_connector/orm/exercises/db_interactions.py
@@ -0,0 +1,8 @@
+"""
+Create the same logic as ./db_connector/sqlite3/db_interactions.py
+But this time using sqlalchemy.
+
+You have a example using sqlalchmey in the sqlalchemy_introduction.py file
+
+The goal of this exercise is to translate connector sql code into sqlalchemy code.
+"""
